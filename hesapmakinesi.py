8bdc80c7b85a7b4f37e0207f38207397890db251
sakgun165 <32776568+sakgun165@users.noreply.github.com>
2019-03-24 19:33:15 +0300

    Python Hesap Makinesi

---
+++ hesapmakinesi.py
@@ -0,0 +1,27 @@
+import time
+print("""
+1-Toplama->+
+2-Çıkarma->-
+3-Çarpma ->x
+4->Bölme ->/
+İşleminizi giriniz(örn:2+5)     
+""")
+liste=list()
+while(True):
+    islem=input("İşlemi giriniz:")
+    yedek1=int(islem[0])
+    yedek2=int(islem[-1])
+    liste=[yedek1,islem[1],yedek2]
+    if(liste[1]=="+"):
+        print(liste[0]+liste[-1])
+    elif(liste[1]=="-"):
+        print(liste[0]-liste[-1])
+    elif(liste[1]=="x"):
+        print(liste[0]*liste[-1])
+    elif(liste[1]=="/"):
+        print(liste[0]/liste[-1])
+    else:
+        print("Lütfen geçerli bir işlem giriniz. Program kapatılıyor...")
+        time.sleep(3)
+        break;
+
